eb849275eb90f9e19cf013be9a04696c2db85eb1
Zach <zachcshore@gmail.com>
2023-02-02 10:41:42 -0600

    added sample config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let args = match bg_next::get_args() {
+    let args = match synth::get_args() {
         Ok(args) => args,
         Err(e) => {
             eprintln!("{}", e);
@@ -11,7 +11,7 @@ println!("HERE1main");
     if args.save.is_none() {
 
         println!("HERE2main");
-        match bg_next::download_file(args).and_then(bg_next::run) {
+        match synth::download_file(args).and_then(synth::run) {
             Ok(_) => (),
             Err(e) => {
                 eprintln!("{}", e);
@@ -20,7 +20,7 @@ println!("HERE1main");
         }
     } else {
 
-        match bg_next::save(args.save.unwrap()) {
+        match synth::save(args.save.unwrap()) {
             Ok(_res) => {
             
                 println!("HERE3main");
